f0b8b4444416e703f8d8d32a634dc78cc29742c0
Gabriel Saiz <saiz@xxxlgroup.com>
2023-05-25 16:36:45 +0200

    Rework http handler

--- elasticfacker.go
+++ elasticfacker.go
@@ -1,24 +1,45 @@
 package elasticfacker
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"strings"
 )
 
-func NewInMemoryElasticsearch(mock *MockMethods) *InMemoryElasticsearch {
+const (
+	HeaderContentType     = "Content-Type"
+	HeaderXElasticProduct = "X-Elastic-Product"
+)
+
+func NewInMemoryElasticsearch() *InMemoryElasticsearch {
 	return &InMemoryElasticsearch{
 		indices: make(map[string]map[string]interface{}),
-		aliases: make(map[string]string),
-		mock:    mock,
-		server:  &http.Server{},
+		aliases: make(map[string]interface{}),
+		//mock:    mock,
+		//server:  &http.Server{},
 	}
 }
 
+func (es *InMemoryElasticsearch) SetMockMethods(mock *MockMethods) {
+	es.mock = mock
+}
+
 func (es *InMemoryElasticsearch) Start(address string) {
+	go es.startServer(address)
+}
+
+func (es *InMemoryElasticsearch) startServer(address string) {
 	r := mux.NewRouter()
-	r.HandleFunc("/", es.handleRequest)
+	r.HandleFunc("/", es.handleRoot).Methods("GET")
+	r.HandleFunc("/{indexName}", es.handleIndicesExists).Methods("HEAD")                             //esapi.IndicesExistsRequest
+	r.HandleFunc("/{indexName}", es.handleIndicesCreate).Methods("PUT")                              //esapi.IndicesCreateRequest
+	r.HandleFunc("/_cat/indices/{indexNamePattern}", es.handleCatIndices).Methods("GET")             //esapi.CatIndicesRequest
+	r.HandleFunc("/{indexName}/_alias", es.handleIndicesGetAlias).Methods("GET")                     //esapi.IndicesGetAliasRequest
+	r.HandleFunc("/{indexName}", es.handleIndicesDelete).Methods("DELETE")                           //esapi.IndicesDeleteRequest
+	r.HandleFunc("/_alias/{aliasName}", es.handleIndicesGetAlias).Methods("GET")                     //esapi.IndicesGetAliasRequest
+	r.HandleFunc("/{indexName}/_aliases/{aliasName}", es.handleIndicesDeleteAlias).Methods("DELETE") //esapi.IndicesDeleteAliasRequest
+	r.HandleFunc("/{indexName}/_aliases/{aliasName}", es.handleIndicesPutAlias).Methods("PUT")       //esapi.IndicesPutAliasRequest
 
 	es.server = &http.Server{
 		Addr:    address,
@@ -39,47 +60,64 @@ func (es *InMemoryElasticsearch) Stop() {
 	}
 }
 
-func (es *InMemoryElasticsearch) handleRequest(w http.ResponseWriter, r *http.Request) {
-	splitPath := strings.Split(r.URL.Path, "/")
-	if len(splitPath) < 2 {
-		http.Error(w, "Invalid request", http.StatusBadRequest)
-		return
-	}
+func (es *InMemoryElasticsearch) handleRoot(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set(HeaderContentType, "application/json")
+	w.Header().Set(HeaderXElasticProduct, "Elasticsearch")
+	json.NewEncoder(w).Encode(map[string]string{
+		"name":         "elasticsearch-simulator",
+		"version":      "8.0.0",
+		"cluster_name": "elasticsearch-simulator-cluster",
+		"cluster_uuid": "fKg7K_YTQH6pG5-VzF7nZQ",
+		"tagline":      "You Know, for Search",
+	})
+}
 
-	indexOrAlias := splitPath[1]
-	var response *MockMethods
-	switch {
-	case r.Method == http.MethodHead && len(splitPath) == 2:
-		// IndicesExistsRequest
-		response = es.IndexExists(indexOrAlias)
-	case r.Method == http.MethodPut && len(splitPath) == 2:
-		// IndicesCreateRequest
-		response = es.CreateIndex(indexOrAlias)
-	case r.Method == http.MethodGet && len(splitPath) == 3 && splitPath[2] == "_alias":
-		// IndicesGetAliasRequest (by index)
-		response = es.GetAlias(indexOrAlias)
-	case r.Method == http.MethodGet && len(splitPath) == 3 && splitPath[1] == "_alias":
-		// IndicesGetAliasRequest (by alias)
-		response = es.GetAlias(splitPath[2])
-	case r.Method == http.MethodGet && len(splitPath) == 4 && splitPath[2] == "_cat" && splitPath[3] == "indices":
-		// CatIndicesRequest
-		response = es.IndexExists(indexOrAlias)
-	case r.Method == http.MethodDelete && len(splitPath) == 2:
-		// IndicesDeleteRequest
-		response = es.DeleteIndex(indexOrAlias)
-	case r.Method == http.MethodDelete && len(splitPath) == 4 && splitPath[2] == "_aliases":
-		// IndicesDeleteAliasRequest
-		response = es.DeleteAlias(indexOrAlias, splitPath[3])
-	case r.Method == http.MethodPut && len(splitPath) == 4 && splitPath[2] == "_aliases":
-		// IndicesPutAliasRequest
-		response = es.PutAlias(indexOrAlias, splitPath[3])
-	default:
-		response = &MockMethods{
-			StatusCode:   400,
-			BodyAsString: `{"error": "Invalid request"}`,
-		}
-	}
+func (es *InMemoryElasticsearch) handleIndicesExists(w http.ResponseWriter, r *http.Request) {
+	indexName := mux.Vars(r)["indexName"]
+	response := es.IndexExists(indexName)
+	es.writeResponse(w, response)
+}
 
+func (es *InMemoryElasticsearch) handleIndicesCreate(w http.ResponseWriter, r *http.Request) {
+	indexName := mux.Vars(r)["indexName"]
+	response := es.CreateIndex(indexName)
+	es.writeResponse(w, response)
+}
+
+func (es *InMemoryElasticsearch) handleCatIndices(w http.ResponseWriter, r *http.Request) {
+	indexNamePattern := mux.Vars(r)["indexNamePattern"]
+	response := es.IndexExists(indexNamePattern)
+	es.writeResponse(w, response)
+}
+
+func (es *InMemoryElasticsearch) handleIndicesGetAlias(w http.ResponseWriter, r *http.Request) {
+	indexName := mux.Vars(r)["indexName"]
+	response := es.GetAlias(indexName)
+	es.writeResponse(w, response)
+}
+
+func (es *InMemoryElasticsearch) handleIndicesDelete(w http.ResponseWriter, r *http.Request) {
+	indexName := mux.Vars(r)["indexName"]
+	response := es.DeleteIndex(indexName)
+	es.writeResponse(w, response)
+}
+
+func (es *InMemoryElasticsearch) handleIndicesDeleteAlias(w http.ResponseWriter, r *http.Request) {
+	indexName := mux.Vars(r)["indexName"]
+	aliasName := mux.Vars(r)["aliasName"]
+	response := es.DeleteAlias(indexName, aliasName)
+	es.writeResponse(w, response)
+}
+
+func (es *InMemoryElasticsearch) handleIndicesPutAlias(w http.ResponseWriter, r *http.Request) {
+	indexName := mux.Vars(r)["indexName"]
+	aliasName := mux.Vars(r)["aliasName"]
+	response := es.PutAlias(indexName, aliasName)
+	es.writeResponse(w, response)
+}
+
+func (es *InMemoryElasticsearch) writeResponse(w http.ResponseWriter, response *MockMethods) {
+	w.Header().Set(HeaderXElasticProduct, "Elasticsearch")
 	w.WriteHeader(response.StatusCode)
 	_, _ = w.Write([]byte(response.BodyAsString))
 }
